5ccea881483b8c5ae9465b59de1a8b240cd37039
violentiy1997 <violentiy1997@mail.ru>
2023-06-13 16:49:32 +0300

    add basket in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaShoppingCart } from "react-icons/fa"
 
 export default function Header() {
+
+   let [cartOpen, setCartOpen] = useState(false)
+
    return (
       <header>
          <div>
             <span className='logo'>House Staff</span>
+            <FaShoppingCart onClick={() => setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`} />
+            {cartOpen && (
+               <div className='shop-cart'></div>
+            )}
             <ul className='nav'>
                <li>Про нас</li>
                <li>Контакты</li>
