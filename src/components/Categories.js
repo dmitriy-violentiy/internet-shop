be8d8f64d2edd7da2906924eed2dfde872ba4190
violentiy1997 <violentiy1997@mail.ru>
2023-06-15 12:59:31 +0300

    change class component on functional component in Categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,34 +1,27 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-export class Categories extends Component {
-   constructor (props) {
-      super(props)
-      this.state = {
-         categories: [
-            {
-               key: 'all',
-               name: 'Все'
-            },
-            {
-               key: 'big bag',
-               name: 'Большие сумки'
-            },
-            {
-               key: 'small bag',
-               name: 'Маленькие сумки'
-            }
-         ]
+export default function Categories(props) {
+   
+   let [categories, setCategories] = useState([
+      {
+         key: 'all',
+         name: 'Все'
+      },
+      {
+         key: 'big bag',
+         name: 'Большие сумки'
+      },
+      {
+         key: 'small bag',
+         name: 'Маленькие сумки'
       }
-   }
-   render() {
-      return (
-         <div className='categories'>
-            {this.state.categories.map(el => (
-               <div key={el.key} onClick={() => this.props.chooseCategory(el.key)}>{el.name}</div>
-            ))}
-         </div>
-      )
-   }
-}
+   ])
 
-export default Categories
+   return (
+      <div className='categories'>
+         {categories.map(el => (
+            <div key={el.key} onClick={() => props.chooseCategory(el.key)}>{el.name}</div>
+         ))}
+      </div>
+   )
+}
